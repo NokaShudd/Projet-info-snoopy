cff4ae1ba1b07b1d3c80ef9a0d208588246607f1
herodrius <pierre.juramie@edu.ece.fr>
2023-11-27 18:11:03 +0100

    update mouvement snoopy + grille

--- main.c
+++ main.c
@@ -3,6 +3,7 @@
 #include "utils\size.h"
 #include "utils\keysHandler.h"
 #include <unistd.h>
+#include "Snoopy/snoopyMovement.h"
 
 // ne pas supprimer (support macos / linux (j'espère))
 #ifndef _WIN32
@@ -20,33 +21,8 @@ int main(){
     refresh();
 
     #endif
-
-    // exemple
-    colorPrintf(newAttr(red, yellow), "test\n");
-
-    // toute les couleurs
-    for (int i = 0; i < 8; i++){
-        for (int j = 0; j < 8; j++){
-            colorPrintf(newAttr(i, j), "test");
-        }
-        printf("\n");
-    }
-    
-
-    kpStruct kps = {none, 0};
-
-    void* hT = getAsyncKey(&kps);
-
-    while (1){
-        colorPrintf(newAttr(white, black), "Key pressed is : %c\n", keyToChar(kps.k));
-
-        if (kps.k == q) {kps.shouldStop = 1; break;}
-
-
-        sleep(1);
-    }
-    
-    closeThread(hT);
+    grille();
+    Movement();
 
     return 0;
 }
